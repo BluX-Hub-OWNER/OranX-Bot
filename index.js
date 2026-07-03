7c1d2b37e6f2d1ffe1a1b0f849855c69390a266d
BluX-Hub-OWNER <aarontramireztijoistm@gmail.com>
2026-07-03 16:11:33 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits, Events } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.once(Events.ClientReady, () => {
+  console.log(`Conectado como ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
